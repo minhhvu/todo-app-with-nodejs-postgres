c05a429eea385217af418410d63b654a4420dc59
minhh <minhhaivuhd@gmail.com>
2020-07-29 15:05:55 -0400

    create api to update a todo as done

--- routes/todos.js
+++ routes/todos.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 const {getAllTodos, createTodo, updateTodo} = require('../controllers/todo')
 
+//@route /api/v1/todos
+
 router.get('/', getAllTodos);
 router.post('/', createTodo);
 router.put('/:id', updateTodo);
+
+//Api to update a todo as done
+router.put('/done/:id', function (req, res, next) {
+  req.body.isDone = true;
+  next()
+}, updateTodo)
+
+//Api to assign multiple users into a todo
 
 module.exports = router
