663bc7bbc0a8fced3b0708948a6758a0447910b0
minhh <minhhaivuhd@gmail.com>
2020-08-06 11:24:48 -0400

    remove all dir

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var {getAllUsers} = require('../controllers/user')
-
-/* GET users listing. */
-router.get('/', getAllUsers);
-
-module.exports = router;
